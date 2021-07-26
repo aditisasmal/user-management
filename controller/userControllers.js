ed84c9f3f7c50753f70852247f5fe6887b30de0d
Aditi Sasmal <aditi.sasmal@codeclouds.in>
2021-07-26 12:29:50 +0530

    working UI section

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -131,7 +131,7 @@ exports.protected = async(req,res,next) =>
 
 exports.logout = (req, res) => {
   res.cookie('jwt', 'loggedout', {
-    expires: new Date(Date.now() + 10 * 1000),
+    expires: new Date(Date.now() - 10 * 1000),
     httpOnly: true
   });
   res.status(200).json({ status: 'success' });
